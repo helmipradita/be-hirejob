173a80d43d3015ca1c82b469b13b19976227e855
Helmi Pradita <helmipraditaa@gmail.com>
2022-11-23 19:18:57 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require(`cors`);
+require(`dotenv`).config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+
+// app.all('*', (req, res, next) => {
+//   response(res, 404, false, null, '404 Not Found');
+// });
+
+app.use('/', (req, res, next) => {
+  res.status(200).json({ status: 'success', statusCode: 200 });
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Example app listening on port ${port}`);
+});
