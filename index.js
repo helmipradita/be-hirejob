5b263a258912c2973339a39e0782bceb931db10a
Helmi Pradita <helmipraditaa@gmail.com>
2022-11-23 21:30:42 +0700

    delete file db.js and change config postgreSQL

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express');
 const cors = require(`cors`);
 require(`dotenv`).config();
 
+const { Client } = require('pg');
+const connectionString = process.env.PG_CONNECT;
+const client = new Client({
+  connectionString,
+});
+
+client.connect((err) => {
+  if (err) {
+    console.log('<:: PostgreSQL Client Error', err);
+  } else {
+    console.log(`::> PostgreSQL Client Connected`);
+  }
+});
+
 const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
 
-// app.all('*', (req, res, next) => {
-//   response(res, 404, false, null, '404 Not Found');
-// });
+app.all('*', (req, res, next) => {
+  res.status(404).json({ status: 'error', statusCode: 404 });
+});
 
 app.use('/', (req, res, next) => {
   res.status(200).json({ status: 'success', statusCode: 200 });
