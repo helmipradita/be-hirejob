b38429c0b0e3b0747dd3fd07f4e5e0e5dfb24962
Helmi Pradita <helmipraditaa@gmail.com>
2022-11-24 10:28:30 +0700

    finisih API roles

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const RolesRouter = require('../routes/roles');
+
+router.use('/roles', RolesRouter);
 
 module.exports = router;
