e90f969da7cb0f5bf910fe07dbc2cb36725d6c34
alvinjamal <alvinjamalazkya@gmail.com>
2022-11-26 22:59:32 +0700

    change type data phone_number, otp

--- query.sql
+++ query.sql
@@ -1,11 +1,6 @@
 -- Active: 1669270835800@@topsy.db.elephantsql.com@5432@ryfxunak@public
 create table test(id INT, name VARCHAR(35));
 
-CREATE TABLE users(id VARCHAR NOT NULL PRIMARY KEY, name VARCHAR NOT NULL, email TEXT NOT NULL,  phone_number INT, company VARCHAR(50), position VARCHAR(50), password TEXT NOT NULL, role VARCHAR, otp INT, verif VARCHAR(1));
-
-
-
-
-
+CREATE TABLE users(id VARCHAR NOT NULL PRIMARY KEY, name VARCHAR NOT NULL, email TEXT NOT NULL,  phone_number VARCHAR, company VARCHAR(50), position VARCHAR(50), password TEXT NOT NULL, role VARCHAR, otp VARCHAR, verif VARCHAR(1));
 
 
