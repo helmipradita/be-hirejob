4808f53550bba4d86d23147343786c7280e41e31
Helmi Pradita <helmipraditaa@gmail.com>
2022-11-28 13:40:54 +0700

    delete user module

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const RolesRouter = require("../routes/roles");
-const UsersRouter = require("../routes/users");
+const RolesRouter = require('../routes/roles');
 
 const CompanyRouter = require('../routes/company');
 const EmployeeRouter = require('../routes/employee');
 
-
-router.use("/roles", RolesRouter);
-router.use("/users", UsersRouter);
+router.use('/roles', RolesRouter);
 router.use('/company', CompanyRouter);
 router.use('/employee', EmployeeRouter);
 
